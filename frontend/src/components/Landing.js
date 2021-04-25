6f5d76b269d8e2b9b39b981425f3c7ded8210eb0
jpandh <jasanpreet@g.ucla.edu>
2021-04-25 11:21:01 -0700

    formatting

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -15,37 +15,37 @@ function Landing() {
       <p id="corevals">
       Our Core Values
       </p>
-      <div className="row">
-      <div className="column">
-        <div className="card">
+      <div className="rowl">
+      <div className="columnl">
+        <div className="cardl">
           Involvement 
         </div>
       </div>
-      <div className="column">
-        <div className="card">
+      <div className="columnl">
+        <div className="cardl">
           Responsiblity
         </div>
       </div>
-      <div className="column">
-        <div className="card">
+      <div className="columnl">
+        <div className="cardl">
           Connection
         </div>
       </div>
     </div>
-    <div className="row2">
-      <div className="column2">
-        <div className="card">
+    <div className="row2l">
+      <div className="column2l">
+        <div className="cardl">
         Encouraging children to explore finances and reach new milestones, all while
         helping children make a purposeful decisions about their pocket money. 
         </div>
       </div>
-      <div className="column2">
-        <div className="card">
+      <div className="column2l">
+        <div className="cardl">
           Children choose their own goals, and how they want to achieve them. 
         </div>
       </div>
-      <div className="column2">
-        <div className="card">
+      <div className="column2l">
+        <div className="cardl">
         Building connections by strengthening the family bond 
         and having more joy through everyday communication
         </div>
